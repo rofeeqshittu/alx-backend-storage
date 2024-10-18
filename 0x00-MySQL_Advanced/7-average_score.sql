57648c11cf529c2f6a5f0720485b78579153cab5
rofeeqshittu <rofeeqshittu21@gmail.com>
2024-10-18 23:24:24 +0100

    Average score..

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -9,11 +9,11 @@ BEGIN
 	-- Compute the average score for the user
 	SELECT AVG(score) INTO avg_score
 	FROM corrections
-	WHERE user_id = user_id;
+	WHERE corrections.user_id = user_id;
 
 	-- Update the user's average_score
 	UPDATE users
-	SET average_score = avg_score
-	WHERE id = user_id;
+	SET users.average_score = avg_score
+	WHERE users.id = user_id;
 END //
 DELIMITER ;
